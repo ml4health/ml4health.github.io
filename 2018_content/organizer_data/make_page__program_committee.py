3db07daf9b9d79b2cbbe3f2f3ff3d8f4e2f9e15f
Irene Chen <iychen6@gmail.com>
2018-11-08 17:51:51 -0500

    Just SPC for now

--- 2018_content/organizer_data/make_page__program_committee.py
+++ 2018_content/organizer_data/make_page__program_committee.py
@@ -49,26 +49,30 @@ out_md_str += "</div>\n"
 
 # Existing PC Members code
 #n_per_row = 100
-out_md_str += \
-"""
-Many thanks to the 100+ members of our program committee who reviewed submitted papers.
 
-<div class="row display-flex" style="display:flex; display:-webkit-flex;  flex-wrap:wrap;">
-"""
+# Irene 11/08/18: omit PC for now.
 
-for item_id, row_obj in enumerate(pc_df.itertuples()):
-    row_dict = row_obj.__dict__
-    item_str = item_template_str + ""
-    for key, val in row_dict.items():
-        default_val = ""
-        cur_val = str(val)
-        if len(cur_val) == '' or cur_val == 'nan':
-            cur_val = default_val
-        item_str = item_str.replace("{{%s}}" % str(key), cur_val)
-    out_md_str += item_str
+# out_md_str += \
+# """
+# Many thanks to the 100+ members of our program committee who reviewed submitted papers.
+
+# <div class="row display-flex" style="display:flex; display:-webkit-flex;  flex-wrap:wrap;">
+# """
+
+# for item_id, row_obj in enumerate(pc_df.itertuples()):
+#     row_dict = row_obj.__dict__
+#     item_str = item_template_str + ""
+#     for key, val in row_dict.items():
+#         default_val = ""
+#         cur_val = str(val)
+#         if len(cur_val) == '' or cur_val == 'nan':
+#             cur_val = default_val
+#         item_str = item_str.replace("{{%s}}" % str(key), cur_val)
+#     out_md_str += item_str
 
 
-out_md_str += "</div>\n"
+# out_md_str += "</div>\n"
+
 
 with open("../pages/program_committee.md", 'w') as f:
     f.write(out_md_str)

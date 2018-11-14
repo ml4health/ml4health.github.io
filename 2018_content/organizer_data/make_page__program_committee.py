9a3d4fa723f2d9e2458df11fcea24023c45043eb
Irene Chen <iychen6@gmail.com>
2018-11-14 16:15:06 -0500

    Add program committee

--- 2018_content/organizer_data/make_page__program_committee.py
+++ 2018_content/organizer_data/make_page__program_committee.py
@@ -29,6 +29,7 @@ out_md_str += (
 # New SPC Members code
 out_md_str += \
 """
+<h3>Senior Program Committee</h3>
 Many thanks to the Senior Program Committee their help and nomination of program committee members.
 
 <div class="row display-flex" style="display:flex; display:-webkit-flex;  flex-wrap:wrap;">
@@ -50,28 +51,29 @@ out_md_str += "</div>\n"
 # Existing PC Members code
 #n_per_row = 100
 
-# Irene 11/08/18: omit PC for now.
+out_md_str += \
+"""
+<br><br>
+<h3>Program Committee</h3>
 
-# out_md_str += \
-# """
-# Many thanks to the 100+ members of our program committee who reviewed submitted papers.
+Many thanks to the 100+ members of our program committee who reviewed submitted papers.
 
-# <div class="row display-flex" style="display:flex; display:-webkit-flex;  flex-wrap:wrap;">
-# """
+<div class="row display-flex" style="display:flex; display:-webkit-flex;  flex-wrap:wrap;">
+"""
 
-# for item_id, row_obj in enumerate(pc_df.itertuples()):
-#     row_dict = row_obj.__dict__
-#     item_str = item_template_str + ""
-#     for key, val in row_dict.items():
-#         default_val = ""
-#         cur_val = str(val)
-#         if len(cur_val) == '' or cur_val == 'nan':
-#             cur_val = default_val
-#         item_str = item_str.replace("{{%s}}" % str(key), cur_val)
-#     out_md_str += item_str
+for item_id, row_obj in enumerate(pc_df.itertuples()):
+    row_dict = row_obj.__dict__
+    item_str = item_template_str + ""
+    for key, val in row_dict.items():
+        default_val = ""
+        cur_val = str(val)
+        if len(cur_val) == '' or cur_val == 'nan':
+            cur_val = default_val
+        item_str = item_str.replace("{{%s}}" % str(key), cur_val)
+    out_md_str += item_str
 
 
-# out_md_str += "</div>\n"
+out_md_str += "</div>\n"
 
 
 with open("../pages/program_committee.md", 'w') as f:
